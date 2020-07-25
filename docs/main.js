0b338ee1a9e111399c615615810ae3a8e28fb03f
AnthonySosa <tony96sosa@hotmail.com>
2020-07-24 22:38:41 -0300

    responsive

--- docs/main.js
+++ docs/main.js
@@ -1,5 +1,9 @@
-const hamburguer = document.querySelector('.hamburguer')
+const hamburguer = document.querySelector('.hamburguer');
+const nav_list = document.querySelector('.nav-list');
 
 hamburguer.addEventListener('click',()=>{
     hamburguer.classList.toggle("active");
+});
+hamburguer.addEventListener('click',()=>{
+    nav_list.classList.toggle("active");
 })
